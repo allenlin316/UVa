b6eea8e6470ac21a79876e4df07cd9dc03dde08e
allenlin316 <allenlin316@gmail.com>
2021-08-15 00:46:25 +0800

    modify wrong_typing

--- CPE-20181218/10550-Combination Lock/10550-Combination Lock.cpp
+++ CPE-20181218/10550-Combination Lock/10550-Combination Lock.cpp
@@ -3,12 +3,12 @@ using namespace std;
 
 int main()
 {
-    int num[4], angle, fiveRotate=1080;
+    int num[4], angle, threeRotate=1080;
     while(cin >> num[0] >> num[1] >> num[2] >> num[3]){
         if(num[0] == 0 && num[1] == 0 && num[2] == 0 && num[3] == 0)   break;
         angle = (num[0] - num[1] + 40)%40 + (num[2] - num[1] + 40)%40 +
                         (num[2] - num[3] + 40)%40;
-        angle = angle*9 + fiveRotate;
+        angle = angle*9 + threeRotate;
         cout << angle << endl;
     }
 }
